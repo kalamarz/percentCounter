a3e19a506d49c30adbc1d168751c1d2bf4154df7
Magdalena <kalamarz.magda@gmail.com>
2020-10-02 21:26:45 +0200

    counter

---
+++ index.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    document.querySelector('button').addEventListener("click", function() {
+        const startPrice = document.querySelector('input[name="startPrice"]').value;
+        const percent = document.querySelector('input[name="percent"]').value;  
+        const result = startPrice - (startPrice * (percent/100));
+        document.getElementById("price").innerHTML = result;
+      });
+});
